a7b5d1fd5c8eeda9735c64e838c64ca257d3622a
Sergei Pechinov <spechiov@gmail.com>
2024-02-15 09:14:36 +0100

    Add: Form and CodeResult in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
+import {Form} from "./containers/Form";
+import {useState} from "react";
+import {ResultCode} from "./containers/ResultCode";
 
 function App() {
+    const [code, setCode] = useState('');
+
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    )
+        <>
+            <Form onSubmit={setCode} />
+            <ResultCode code={code} />
+        </>
+    );
 }
 
 export default App
